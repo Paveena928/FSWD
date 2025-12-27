33e9be043f3613f96f1cdc85fd67e127ce4cc2ea
Paveena928 <paveenazintra@gmail.com>
2025-12-27 21:26:43 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "./Fullstack/React/Dashboard";
+
+function App() {
+  return (
+    <div>
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
