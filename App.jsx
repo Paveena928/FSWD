ca73c1ecc9ed4bf0b811631e13081b0637f83bc6
Paveena928 <paveenazintra@gmail.com>
2025-12-27 20:00:28 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Workshop from "./Fullstack/React/Workshop";
+
+
+function App() {
+  return (
+    <div>
+      <Workshop/>
+    </div>
+  );
+}
+
+export default App;
