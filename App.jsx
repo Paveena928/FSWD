24482dc77284920009fa9a312e704cfdfde167e3
Paveena928 <paveenazintra@gmail.com>
2025-12-27 21:20:50 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Product from "./Fullstack/React/Product";
+
+function App() {
+  return (
+    <div>
+      <Product/>
+    </div>
+  );
+}
+
+export default App;
