2036788f9877b8d9330e9f77c9f1c4e283a26253
Paveena928 <paveenazintra@gmail.com>
2025-12-27 21:05:50 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Notification from "./Fullstack/React/Notification.jsx";
+
+
+function App() {
+  return (
+    <div>
+      <Notification/>
+    </div>
+  );
+}
+
+export default App;
