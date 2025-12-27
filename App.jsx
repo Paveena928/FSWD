eb3b87182cf452719760532d1a6d9cefa47ef4e3
Paveena928 <paveenazintra@gmail.com>
2025-12-27 22:00:20 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Multipage from "./Fullstack/React/Multipage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Multipage />
+    </BrowserRouter>
+  );
+}
+
+export default App;
