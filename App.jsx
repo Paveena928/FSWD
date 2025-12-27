5bf8b3d26bc91da9e51d12f86fa1ed7ce3bca9c4
Paveena928 <paveenazintra@gmail.com>
2025-12-27 20:29:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Feedback from "./Fullstack/React/Feedback.jsx";
+
+
+
+function App() {
+  return (
+    <div>
+      <Feedback/>
+    </div>
+  );
+}
+
+export default App;
