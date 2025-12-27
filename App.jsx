17b4a60b790a938781f583db7c46d1fae3743eca
Paveena928 <paveenazintra@gmail.com>
2025-12-27 20:18:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Authentication from "./Fullstack/React/Authentication";
+
+
+
+function App() {
+  return (
+    <div>
+      <Authentication/>
+    </div>
+  );
+}
+
+export default App;
